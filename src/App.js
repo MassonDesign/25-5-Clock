b538dc0fc371dcc970e1cb9c0f4f1f2a47da639a
frederic masson <fredericmasson@air-de-frederic.lan>
2021-10-12 17:47:53 -0400

    adding useState and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import Break from "./components/Break";
+import Controls from "./components/Controls";
+import Display from "./components/Display";
+import Session from "./components/Session";
+import { useState } from "react";
+
 import "./index.css";
 
 function App() {
-	return <div></div>;
+	const [sessionLenght, setSessionLenght] = useState(5);
+	const [breakLenght, setbreakLenght] = useState(25);
+	const [display, setDisplay] = useState("25:00:00");
+
+	return (
+		<div>
+			<header className='text-center'>25 + 5 clock</header>
+
+			<main className='flex bg-gray-200 justify-center items-center h-screen w-full m-auto'>
+				<div className='flex flex-col bg-gray-900 h-5/6 w-80 rounded-3xl shadow-2xl'>
+					<Display display={display} />
+
+					<div className='flex justify-center items-center -mt-10 space-x-6'>
+						<Break breakLenght={breakLenght} />
+						<Session sessionLenght={sessionLenght} />
+					</div>
+
+					<Controls />
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+function Display({ display }) {
+	return (
+		<div className='flex items-center justify-center text-white text-center border border-red-400 rounded-full m-auto h-60 w-60'>
+			<p>{display}</p>
+		</div>
+	);
+}
+
+export default Display;
